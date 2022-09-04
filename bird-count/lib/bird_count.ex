0fb759024ae401d2df17900066415879a1ecfea7
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-09-04 20:56:17 -0300

    feat: refactor bird count using recursion

--- bird-count/lib/bird_count.ex
+++ bird-count/lib/bird_count.ex
@@ -13,16 +13,33 @@ defmodule BirdCount do
     end
   end
 
-  # FIXME: use recursion
-  def has_day_without_birds?(list) do
-    if Enum.find_value(list, &(&1 == 0)), do: true, else: false
+  def has_day_without_birds?([]), do: false
+
+  def has_day_without_birds?([hd | tl]) do
+    if hd == 0, do: true, else: has_day_without_birds?(tl)
   end
 
   def total(list) do
-    Enum.reduce(list, 0, &(&1 + &2))
+    sum(list)
   end
 
+  def sum([]), do: 0
+
+  def sum([head | tail]), do: head + sum(tail)
+
   def busy_days(list) do
-    length(Enum.filter(list, &(&1 >= 5)))
+    length(filter(list, &(&1 >= 5)))
   end
+
+  def filter([head | tail], func) do
+    result = func.(head)
+
+    if result do
+      [head | filter(tail, func)]
+    else
+      filter(tail, func)
+    end
+  end
+
+  def filter([], _func), do: []
 end
