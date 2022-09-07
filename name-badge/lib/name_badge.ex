e2d56d87ad8e8c280c7bba96956bb6975292ed70
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-09-07 17:41:08 -0300

    feat: german-sysadmin

--- name-badge/lib/name_badge.ex
+++ name-badge/lib/name_badge.ex
@@ -1,17 +1,17 @@
 defmodule NameBadge do
-  def print(id, name, department) do
-    if is_nil(id) do
-      if is_nil(department) do
-        "#{name} - OWNER"
-      else
-        "#{name} - #{String.upcase(department)}"
-      end
+  def print(nil, name, department) do
+    if is_nil(department) do
+      "#{name} - OWNER"
     else
-      if is_nil(department) do
-        "[#{id}] - #{name} - OWNER"
-      else
-        "[#{id}] - #{name} - #{String.upcase(department)}"
-      end
+      "#{name} - #{String.upcase(department)}"
     end
+  end
+
+  def print(id, name, nil) do
+    "[#{id}] - #{name} - OWNER"
+  end
+
+  def print(id, name, department) do
+    "[#{id}] - #{name} - #{String.upcase(department)}"
   end
 end
